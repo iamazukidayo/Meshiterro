718e0342ca4b5fa5382922833b82bb9f56394959
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-05-09 03:46:16 +0000

    SNS1

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
  #onlyオプションを使うと、生成されるルーティングを限定する。
  root to: "homes#top"
  
