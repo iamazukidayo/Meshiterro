5e5cd2a861bdae6893131dae74216254d01b58aa
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-05-09 04:16:41 +0000

    comment

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,7 @@
 class PostImage < ApplicationRecord
   has_one_attached :image
    belongs_to :user
+   has_many :post_comments, dependent: :destroy
    #belongs_to :userで紐つけてる
    #PostImageモデルに関連付けられるのは一個のユーザーモデルなので、単数系のuser
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
   #get 'post_images/show'
   #get 'users/about' => 'users#about', as:'about'
   devise_for :users
+  root to: "homes#top"
+   get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
+    resources :post_comments, only: [:create]
+  end
   resources :users, only: [:show, :edit, :update]
  #onlyオプションを使うと、生成されるルーティングを限定する。
- root to: "homes#top"
+ 
  
 end
 
